f433c88f7572533edace6ab0df66265a490a42b4
Flanks255 <32142731+Flanks255@users.noreply.github.com>
2023-07-19 09:17:58 -0500

    Added the ability to filter items going into the graves with the item tag, "simplygraves:tags/items/no_grave", Closes #1

---
+++ src/main/java/com/flanks255/simplygraves/data/SGItemTags.java
@@ -0,0 +1,22 @@
+package com.flanks255.simplygraves.data;
+
+import com.flanks255.simplygraves.SimplyGraves;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.util.concurrent.CompletableFuture;
+
+public class SGItemTags extends ItemTagsProvider {
+    public SGItemTags(DataGenerator dataGenerator, CompletableFuture<HolderLookup.Provider> something, BlockTagsProvider blockTagProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(dataGenerator.getPackOutput(), something, blockTagProvider.contentsGetter(), SimplyGraves.MODID, existingFileHelper);
+    }
+    @Override
+    protected void addTags(@Nonnull HolderLookup.Provider pointless) {
+        getOrCreateRawBuilder(SimplyGraves.NO_GRAVE);
+    }
+}
